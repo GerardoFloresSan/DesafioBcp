6a2fa7bcf4b7739b75ad43e8eee07a7fc6b689c9
Gerardo Flores <gerardo.gabriel.flores@gmail.com>
2020-07-01 16:00:34 -0500

    Entity-Money y Operation

---
+++ app/src/main/java/com/gerardo/desafiobcp/data/entity/MoneyEntity.kt
@@ -0,0 +1,24 @@
+package com.gerardo.desafiobcp.data.entity
+
+import com.squareup.moshi.Json
+import java.io.Serializable
+
+data class MoneyEntity (
+    @Json(name = "MONEY_NAME_COUNTRY") var moneyNameCountry: String = "",
+    @Json(name = "MONEY_NAME") var moneyName: String = "",
+    @Json(name = "TYPE_CHANGE_SALE") var typeChangeSale: Double = 0.0,
+    @Json(name = "TYPE_CHANGE_BUY") var typeChangeBuy: Double = 0.0,
+    @Json(name = "ABBREVIATION_MONEY") var abbreviationMoney: String = "",
+    @Json(name = "URL_FLAG") var urlFlag: String = "",
+    @Json(name = "FACTOR_MONEY") var factoryMoney: String = ""
+) : Serializable {
+    override fun toString(): String {
+        return "MoneyEntity(moneyNameCountry='$moneyNameCountry'," +
+                "moneyName='$moneyName', " +
+                "typeChangeSale='$typeChangeSale', " +
+                "typeChangeBuy='$typeChangeBuy', " +
+                "abbreviationMoney='$abbreviationMoney', " +
+                "urlFlag='$urlFlag'," +
+                "factoryMoney='$factoryMoney')"
+    }
+}
